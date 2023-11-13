246b5b0aaf56ae7a0af5a656e44d8818d855dfe3
ahmadkhan5346 <khansarfraz5346@gmail.com>
2023-11-13 21:34:08 +0530

    create a component and routing

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,14 @@
+
+const Contact = () => {
+  return <>
+
+  <h1>Contact Page</h1>
+  <hr />
+  <p>Contact Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque quibusdam provident culpa accusamus sint quasi illum adipisci. Nostrum, natus assumenda rem aliquam atque accusantium amet incidunt vitae ipsam at sequi aliquid dolor nulla minus ea delectus nobis laborum. Quam ullam voluptatem facilis architecto atque voluptas laboriosam nihil doloribus cum id aliquam assumenda dicta, ut sunt repellat. Quas suscipit tenetur architecto, dicta commodi et possimus soluta iure esse, ea impedit fugiat provident. Fuga maxime consequuntur earum? Tempore odit ratione consectetur accusantium? Unde, reprehenderit accusamus? Enim libero rerum nihil et illum quisquam minus ad possimus quis, maxime reiciendis incidunt at culpa debitis.</p>
+  
+  
+  
+  </>;
+};
+
+export default Contact;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+  return <>
+    <h1>Home Page</h1>
+    <hr />
+    <p>Home Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ut voluptate quasi consequatur illo ipsa nulla minus? Omnis dignissimos qui facere at quis nobis consequatur iusto modi eos quasi ad in, sunt cumque iure! Doloribus nam voluptates, velit aut laboriosam odio laudantium laborum in neque repudiandae possimus amet quis pariatur at porro quisquam quae harum sed, nostrum nisi eos atque consequuntur numquam. Expedita quam quos sed mollitia similique explicabo, delectus repellendus consequatur possimus aliquam, necessitatibus odit consectetur molestiae ab ratione quisquam officia a, quae perspiciatis exercitationem sint cumque ut enim unde. Fugit expedita porro dolorum explicabo, consequuntur ab incidunt nulla!</p>
+
+  </>
+}
+
+export default Home
