c2c240b64e96993988f556f69adaf832c11cda6f
ahmadkhan5346 <khansarfraz5346@gmail.com>
2023-11-14 20:54:44 +0530

    create user login registration and email password

--- Auth Dashboard/auth-ui/src/App.js
+++ Auth Dashboard/auth-ui/src/App.js
@@ -2,6 +2,9 @@ import Contact from "./components/pages/Contact";
 import Home from "./components/pages/Home";
 import Layout from "./components/pages/Layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginReg from "./components/pages/auth/LoginReg";
+import SendPasswordResetEmail from "./components/pages/auth/SendPasswordResetEmail";
+import ResetPassword from "./components/pages/auth/ResetPassword";
 
 
 function App() {
@@ -13,6 +16,9 @@ function App() {
             <Route path="/" element={<Layout />}>
             <Route index element={<Home/>}/>
             <Route path="contact" element={<Contact/>}/>
+            <Route path="login" element={<LoginReg/>}/>
+            <Route path="sendpasswordresetemail" element={<SendPasswordResetEmail/>}/>
+            <Route path="resetpassword" element={<ResetPassword/>}/>
                 
             </Route>
         </Routes>
--- Auth Dashboard/auth-ui/src/components/Navbar.js
+++ Auth Dashboard/auth-ui/src/components/Navbar.js
@@ -9,8 +9,9 @@ const Navbar = () => {
             <Typography variant='h5' component='div' sx={{flexGrow:1}}>
                 Mumbai Coding Club
             </Typography>
-            <Button component={NavLink} to='/' style={({isActive})=>{return{backgroundColor: isActive ? '#6d1b7b':''}}} sx={{color:'white'}}>Home</Button>
-            <Button component={NavLink} to='contact' style={({isActive})=>{return{backgroundColor: isActive ? '#6d1b7b':''}}} sx={{color:'white'}}>Contact</Button>
+            <Button component={NavLink} to='/' style={({isActive})=>{return{backgroundColor: isActive ? '#6d1b7b':''}}} sx={{color:'white', textTransform:'none'}}>Home</Button>
+            <Button component={NavLink} to='contact' style={({isActive})=>{return{backgroundColor: isActive ? '#6d1b7b':''}}} sx={{color:'white', textTransform:'none'}}>Contact</Button>
+            <Button component={NavLink} to='login' style={({isActive})=>{return{backgroundColor: isActive ? '#6d1b7b':''}}} sx={{color:'white', textTransform:'none'}}>Login/Registration</Button>
         </Toolbar>
 
     </AppBar>
